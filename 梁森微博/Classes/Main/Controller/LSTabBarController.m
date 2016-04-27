f267b4b533bd81cbffdbc8ebe0f151f244e6d114
Liangsen <1096359518@qq.com>
2016-04-27 11:19:13 +0800

    6.后台播放音乐

--- 梁森微博/Classes/Main/Controller/LSTabBarController.m
+++ 梁森微博/Classes/Main/Controller/LSTabBarController.m
@@ -61,6 +61,7 @@
 // 每隔三秒就刷新一次，刷新tabBar上的未读数
 - (void)requestUnread
 {
+    NSLog(@"定时刷新。。。");
     _homeVC.tabBarItem.badgeValue = @"1";
     // 未读数显示在应用图标上,前提：注册通知
     [UIApplication sharedApplication].applicationIconBadgeNumber = 3;
